d0cfd80bac8cbf2a84089374293806a2e1820fa4
Khairul122 <khairulhuda242@gmail.com>
2025-03-22 15:57:46 +0700

    add CORS middleware configuration for FastAPI

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 import pymysql
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+# Konfigurasi CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Untuk testing, izinkan semua origin
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 # Konfigurasi koneksi ke MySQL di Clever Cloud
 db_config = {
